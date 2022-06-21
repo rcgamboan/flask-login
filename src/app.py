2416c71496841fff751486e96d9067ae0c695936
rcgamboan <rcgamboan@gmail.com>
2022-06-21 19:22:49 -0400

    agregados metodos para editar y eliminar usuarios, incluida vista para proveedores

--- src/app.py
+++ src/app.py
@@ -4,16 +4,21 @@ from werkzeug.security import check_password_hash , generate_password_hash
 from config import config
 import sqlite3 as sql
 import database as db1
-from model import Usuario
+from model import Usuario, Producto
 
 
 app = Flask(__name__)
 
 # Recibe el username y la contraseña del usuario a crear
 # Se llama a la sesion de SQLAlchemy y se crea el usuario
-def agregarUsuario(username,password,isAdmin):
-    user = Usuario(username, generate_password_hash(password),isAdmin)
+def agregarUsuario(username,password,rol):
+    user = Usuario(username, generate_password_hash(password),rol)
     db1.session.add(user)
+    db1.session.commit()
+
+def agregarProducto(nombre,idProveedor,precio,descripcion = ''):
+    prod = Producto(nombre, idProveedor, precio, descripcion)
+    db1.session.add(prod)
     db1.session.commit()
 
 # Si se trata de acceder al directorio raiz de la pagina se redirecciona a login
@@ -37,10 +42,12 @@ def login():
 
             if check_password_hash(logged_user.password,request.form['password']):
 
-                if logged_user.isAdmin == 0:
+                if logged_user.rol == 0:
                     return redirect(url_for('home'))
-                else:
+                elif logged_user.rol == 1:
                     return redirect(url_for('admin'))
+                else:
+                    return redirect(url_for('proveedor'))
             else:
                 flash("Alguno de los datos es incorrecto")
                 return render_template('auth/login.html')
@@ -57,6 +64,8 @@ def home():
 
 @app.route('/admin/update',methods=['POST'])
 def update():
+    #obtener tipo y con eso crear funciones para eliminar o editar segun corresponda
+    #print(request.form['tipo'])
     actualizarRol(request.form['ID'], request.form['rol'])
     return redirect(url_for('admin'))
 
@@ -70,7 +79,7 @@ def admin():
 
         #si no existe el usuario en la base de datos
         if new_user == None:
-            agregarUsuario(request.form['username'], request.form['password'], request.form['isAdmin'])
+            agregarUsuario(request.form['username'], request.form['password'], request.form['rol'])
             users = obtenerUsuarios()
             return render_template('admin.html',users=users)
 
@@ -81,15 +90,60 @@ def admin():
     else:
         users = obtenerUsuarios()
         return render_template('admin.html',users=users)
-    
+
+# Se obtienen los productos del productor indicado y se pasan como datos para mostrarlos
+@app.route('/proveedor',methods=['GET','POST','PUT'])
+def proveedor():
+    idProveedor = request.form['idProveedor']
+    if request.method=='POST':
+        
+        new_prod = db1.session.query(Producto).filter_by(nombre = request.form['nombre']).first()
+
+        # Tengo que obtener el id del proveedor que solicita agregar el producto y enviarlo como parametro de la funcion
+        if new_prod == None:
+            agregarProducto(request.form['nombre'],idProveedor , request.form['precio'],request.form['descripcion'])
+            productos = obtenerProductos(idProveedor)
+            return render_template('proveedor.html',productos=productos)
+
+        else:
+            flash("Este producto ya existe ")
+            productos = obtenerProductos(idProveedor)
+            return render_template('proveedor.html',productos=productos)   
+    else:
+        idProveedor = request.form['idProveedor']
+        productos = obtenerProductos(idProveedor)
+        return render_template('proveedor.html',productos=productos)
+
+@app.route('/proveedor/update',methods=['POST'])
+def prov_update():
+    return redirect(url_for('proveedor'))
+  
 def actualizarRol(id,newRol):
     user = db1.session.query(Usuario).filter_by(id = id).first()
-    user.isAdmin = newRol
+    user.rol = newRol
     db1.session.commit()
 
 def obtenerUsuarios():
     usuarios = db1.session.query(Usuario).filter_by().all()
     return usuarios
+
+def editarUsuario(id,username,password,rol):
+    user = db1.session.query(Usuario).filter_by(id = id).first()
+    user.username = username
+    user.password = generate_password_hash(password)
+    user.rol = rol
+    db1.session.commit()
+
+# Elimina el usuario por id
+def eliminarUsuario(id):
+    user = db1.session.query(Usuario).filter_by(id = id).first()
+    db1.session.delete(user)
+    db1.session.commit()
+
+# Metodo que retorna los productos correspondientes al proveedor indicado
+def obtenerProductos(id):
+    productos = db1.session.query(Producto).filter_by(proveedor = id).all()
+    return productos
 
 if __name__ == '__main__':
     db1.Base.metadata.create_all(db1.engine)
--- src/model.py
+++ src/model.py
@@ -5,16 +5,38 @@ class Usuario(db.Base):
     __tablename__ = 'usuarios'
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
-    password = Column(String)
-    isAdmin = Column(Integer, default=0)
+    password = Column(String, nullable=False)
+    rol = Column(Integer, default=0, nullable=False)
+    # rol = 0 -> Usuario normal
+    # rol = 1 -> admin
+    # rol = 2 -> proveedor
 
-
-    def __init__(self, username, password, isAdmin = 0):
+    def __init__(self, username, password, rol = 0):
         self.username = username
         self.password = password
-        self.isAdmin = isAdmin
+        self.rol = rol
 
     def __repr__(self):
-        return f'Usuario({self.username}, {self.password}, {self.isAdmin})'
+        return f'Usuario({self.username}, {self.password}, {self.rol})'
     def __str__(self):
         return self.username
+
+class Producto(db.Base):
+    __tablename__ = 'productos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    proveedor = Column(Integer)
+    nombre = Column(String, nullable=False, unique=True)
+    descripcion = Column(String, nullable=True)
+    precio = Column(Integer, default=0, nullable=False)
+
+
+    def __init__(self, nombre, proveedor, precio, descripcion = ''):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+        self.proveedor = proveedor
+
+    def __repr__(self):
+        return f'Producto({self.nombre}, {self.descripcion}, {self.precio})'
+    def __str__(self):
+        return self.nombre
