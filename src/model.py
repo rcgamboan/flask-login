bf2fb0a556b5d972b0f8242690bea517a25c0d11
rcgamboan <rcgamboan@gmail.com>
2022-06-27 13:00:13 -0400

    agrega tabla para productor y tipos de productor

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import database as db
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 class Usuario(db.Base):
     __tablename__ = 'usuarios'
@@ -14,7 +15,7 @@ class Usuario(db.Base):
     rol = Column(Integer, default=0, nullable=False)
     # rol = 0 -> Usuario normal
     # rol = 1 -> admin
-    # rol = 2 -> proveedor
+    # rol = 2 -> productor
 
     def __init__(self, id,username, password, nombres,apellidos,telefonoCelular,telefonoLocal,direccion,rol = 0):
         self.id = id
@@ -32,22 +33,40 @@ class Usuario(db.Base):
     def __str__(self):
         return self.username
 
-class Producto(db.Base):
-    __tablename__ = 'productos'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    proveedor = Column(Integer)
-    nombre = Column(String, nullable=False, unique=True)
-    descripcion = Column(String, nullable=True)
-    precio = Column(Integer, default=0, nullable=False)
+class Productor(db.Base):
+    __tablename__ = 'productor'
+    id = Column(Integer, primary_key=True)
+    nombres = Column(String, nullable=False)
+    apellidos = Column(String, nullable=False)
+    telefonoCelular = Column(Integer, nullable=False)
+    telefonoLocal = Column(Integer, nullable=True)
+    direccion = Column(String, nullable=False)
+    tipo = Column(Integer, ForeignKey("tipoproductor.id"))
 
-
-    def __init__(self, nombre, proveedor, precio, descripcion = ''):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.precio = precio
-        self.proveedor = proveedor
+    def __init__(self, id, nombres,apellidos,telefonoCelular,telefonoLocal,direccion,tipo = ''):
+        self.id = id
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.telefonoCelular = telefonoCelular
+        self.telefonoLocal = telefonoLocal
+        self.direccion = direccion
+        self.tipo = tipo
 
     def __repr__(self):
-        return f'Producto({self.nombre}, {self.descripcion}, {self.precio})'
+        return f'Productor({self.nombres}, {self.apellidos}, {self.tipo})'
     def __str__(self):
-        return self.nombre
+        return self.nombres
+
+class TipoProductor(db.Base):
+    __tablename__ = 'tipoproductor'
+    id = Column(Integer, primary_key=True, autoincrement = True)
+    direccion = Column(String, nullable=False)
+    productor = relationship("Productor")
+
+    def __init__(self, descripcion):
+        self.descripcion = descripcion
+
+    def __repr__(self):
+        return f'TipoProductor({self.descripcion})'
+    def __str__(self):
+        return self.descripcion
